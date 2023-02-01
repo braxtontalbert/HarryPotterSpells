656d5719c1031a448d8f1a8f5c320daa457f6266
braxtontalbert <braxton.talbert@gmail.com>
2023-01-31 19:10:09 -0500

    Original Accio implementation fucntionality

--- Accio.cs
+++ Accio.cs
@@ -113,54 +113,7 @@ namespace WandSpellss
             Transform parent;
             if (Physics.Raycast(item.flyDirRef.transform.position, item.flyDirRef.transform.forward, out hit, float.MaxValue, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore))
             {
-                float raycastDistance;
-                if ((hit.point - item.flyDirRef.transform.position).magnitude < float.MaxValue)
-                {
-                    raycastDistance = (hit.point - item.flyDirRef.transform.position).magnitude;
-
-                    float distanceOut = 2f;
-
-                    float sideC = (float)Math.Sqrt(raycastDistance * raycastDistance + distanceOut * distanceOut);
-
-                    int maxAngle = (int)Math.Round(Math.Asin(distanceOut / sideC));
-
-                    Dictionary<int, double> knownAngleDistance = new Dictionary<int, double>();
-
-                    for (int i = 0; i < maxAngle; i++)
-                    {
-                        if (!knownAngleDistance.ContainsKey(i))
-                        {
-                            knownAngleDistance.Add(i, (raycastDistance / Math.Cos(i)));
-                        }
-                    }
-                    
-
-                    foreach(Item collectedItem in Item.allActive)
-                    {
-                        if(collectedItem)
-                    }
-
-                }
-                else { }
-
-            /*if (Physics.Raycast(item.flyDirRef.transform.position, item.flyDirRef.transform.forward, out hit, float.MaxValue, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore))
-            {
-                float maxHit = (hit.point - item.flyDirRef.position).magnitude;
-                float maxIterative = maxHit / 10;
-                float distanceIterative = 0;
-                float maxDistance = 5f;
-                for (int i = 0; i < maxHit; i++) {
-                    
-                    if(i == (int)Math.Round(maxIterative)) rayPoints.Add(item.flyDirRef.transform.position + (item.flyDirRef.transform.forward * i), distanceIterative);
-                    if (distanceIterative < maxDistance)
-                    {
-                        distanceIterative += 0.3f;
-                    }
-                }
-
-                CustomDebug.Debug("Did hit.");
-                CustomDebug.Debug(hit.collider.gameObject.name);
-
+                
                 parent = hit.collider.gameObject.transform.parent;
                 parentLocal = hit.collider.gameObject;
 
@@ -168,7 +121,7 @@ namespace WandSpellss
 
                 StartAccio(CheckHit(oppositeHand, parentLocal));
                 
-            }*/
+            }
             
 
         }
