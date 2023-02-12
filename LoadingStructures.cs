a86ed8f103bf7ea02601a881b2fcc732c468594e
braxtontalbert <braxton.talbert@gmail.com>
2023-02-12 10:52:16 -0500

    Changed LevelModuleScript to only hold json structures

---
+++ LoadingStructures.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Speech.Recognition;
+using ThunderRoad;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement;
+
+
+namespace WandSpellss
+{
+    public class SpellListData : CustomData
+    {
+
+        public List<JSONSpell> spellList;
+    }
+
+    public class JSONSpell
+    {
+
+        public string name;
+        public string classType;
+
+    }
+}
