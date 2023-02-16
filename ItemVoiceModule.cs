e2ad8a7abaa94ee99381f5b01b78dec28a1462ee
braxtontalbert <braxton.talbert@gmail.com>
2023-02-15 21:27:55 -0500

    basic commit

--- ItemVoiceModule.cs
+++ ItemVoiceModule.cs
@@ -14,13 +14,12 @@ namespace WandSpellss
         public float spellSpeed;
         public bool magicEffect;
         public float expelliarmusPower;
+        
         public override void OnItemLoaded(Item item)
         {
             base.OnItemLoaded(item);
             item.gameObject.AddComponent<SpellEntry>().Setup(spellSpeed, magicEffect, expelliarmusPower);
-            Player.local.creature.gameObject.AddComponent<Soul>();
-
-
+            EventManager.onItemEquip += item1 => { if(item1.Equals(item)) Loader.local.currentlyHeldWands.Add(item1);};
         }
 
     }
